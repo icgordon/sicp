181e6d2bdf5d738fb801ac21419cf5ddd729fb71
Ian Gordon <icgordon@users.noreply.github.com>
2016-07-22 14:49:59 -0600

    Update 1.39.lisp
    Added separate coef function. Also added a helper function to coerce the n parameter to be a float. I was getting tired of forgetting to specify the n value as a float and getting 1/4 as the result.

--- 1.39.lisp
+++ 1.39.lisp
@@ -1,21 +1,27 @@
-;;;; 1.39 Simpson's Rule
+;;;; sicp 1.39 Simpson's Rule
 ;;;; Use the approximation of Simpson's Rule to find the integral of f.
 
 ;; Usage: (simpsons #'function lower upper n)
-;; example: (simpsons #'cube 0 1 100.0)
-;; Notes: if n is entered as an integer, the result will be displayed as a ratio. eEnter n as a float (e.g. 100.0).
+;; example: (simpsons #'cube 0 1 100)
+;; Notes: if n is entered as an integer, the result will be displayed as a ratio.
+
+;; For comparison, use the integral function:
+;; (integral #'cube 0 1 0.01)
 
 (defun simpsons (f a b n)
-  ;(declare (type double-float n))
+  (simpsons-helper f a b (coerce n 'single-float)))
+
+(defun simpsons-helper (f a b n)
   (let ((h (/ (- b a) n) ))
     (labels ((inc (x) (1+ x)))
       (labels ((y (k) (funcall f (+ a (* k h)))))
-        (labels ((term (k)   
-                   (* (cond ((or (= k 0) (= k n)) 1)
-                            ((oddp k) 4)
-                            ((evenp k ) 2))
-                      (y k)) ))
-          (/ (* h (sum #'term 0 #'inc n)) 3))))))
+        (labels ((coef (k)
+                   (cond ((or (= k 0) (= k n)) 1)
+                         ((oddp k) 4)
+                         (t 2))))
+          (labels ((term (k)   
+                   (* (coef k)  (y k)) ))
+          (/ (* h (sum #'term 0 #'inc n)) 3)))))))
 
 
 (defun sum (term a next b)
@@ -23,7 +29,6 @@
         (t (+ (funcall term a) (sum term (funcall next a) next b)))))
 
 
-;; (integral #'cube 0 1 0.001)
 (defun integral (f a b dx)
   (labels ((add-dx (x) (+ x dx)))
     (* (sum f (+ a (/ dx 2.0)) #'add-dx b) dx)))
