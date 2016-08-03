055b918ccef3c997ead294df26ea02e97ff5aa86
Ian Gordon <icgordon@users.noreply.github.com>
2016-08-03 08:11:06 -0600

    Update 1.39.lisp
    A couple of final changes before the presentation.

--- 1.39.lisp
+++ 1.39.lisp
@@ -1,4 +1,4 @@
-;;;; sicp 1.39 Simpson's Rule
+;;;; sicp 1.29 Simpson's Rule
 ;;;; Use the approximation of Simpson's Rule to find the integral of f.
 
 ;; Usage: (simpsons #'function lower upper n)
@@ -8,12 +8,19 @@
 ;; For comparison, use the integral function:
 ;; (integral #'cube 0 1 0.01)
 
+;; To time:
+;; (time (loop for x from 1 to 1000 do (simpsons #'cube 0 1 1000)))
+
+(defun cube (x)
+(* x x x))
+
+
 (defun simpsons (f a b n)
   (simpsons-helper f a b (coerce n 'single-float)))
 
+
 (defun simpsons-helper (f a b n)
   (let ((h (/ (- b a) n) ))
-    (labels ((inc (x) (1+ x)))
       (labels ((y (k) (funcall f (+ a (* k h)))))
         (labels ((coef (k)
                    (cond ((or (= k 0) (= k n)) 1)
@@ -21,7 +28,7 @@
                          (t 2))))
           (labels ((term (k)   
                    (* (coef k)  (y k)) ))
-          (/ (* h (sum #'term 0 #'inc n)) 3)))))))
+          (/ (* h (sum #'term 0 #'1+ n)) 3))))))
 
 
 (defun sum (term a next b)
@@ -34,5 +41,3 @@
     (* (sum f (+ a (/ dx 2.0)) #'add-dx b) dx)))
 
 
-(defun cube (x)
-  (* x x x))
